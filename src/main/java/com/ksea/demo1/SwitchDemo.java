aa88145d6c94a8d82deea190c0da201fe10669b3
ksea55 <wangyang198118@126.com>
2017-02-07 23:53:37 +0800

    java学习以及java常用的面试在resources中

---
+++ src/main/java/com/ksea/demo1/SwitchDemo.java
@@ -0,0 +1,20 @@
+package com.ksea.demo1;
+
+/**
+ * Created by mexican on 2017/2/7.
+ */
+public class SwitchDemo {
+    public static void main(String[] args) {
+        String a = "你好";
+        switch (a) {
+            case "你好":
+                System.out.println("a");
+                break;
+            default:
+                System.out.println("默认值");
+                break;
+
+        }
+
+    }
+}
