c216682e811cbb9d18606983460143903fae250a
ksea55 <wangyang198118@126.com>
2017-03-11 10:06:32 +0800

    泛型的应用，对数据类型的效验，在编译时期就暴露 泛型类的使用 泛型接口的使用 泛型方法的使用 泛型通配符？,<? exntends E>,<? super E>的使用

--- src/main/java/com/ksea/collection/CollectionToFanXing.java
+++ src/main/java/com/ksea/collection/CollectionToFanXing.java
@@ -41,7 +41,7 @@ public class CollectionToFanXing {
 
 
         //这种写法为什么会报错呢，照理说Object是所有类的基类是不应该报错的，从而说明，泛型的数据类型必须前后是一致的数据类型
-        ArrayList<Object> obj3 = new ArrayList<Animal>();
+        //ArrayList<Object> obj3 = new ArrayList<Animal>();
 
         /*?问号，泛型通配符的一种
           这里?问号表示任何数据类型，因此这样写编译都能通过
@@ -58,7 +58,7 @@ public class CollectionToFanXing {
         ArrayList<? extends Animal> obj8 = new ArrayList<Animal>(); //正确，编译通过
         ArrayList<? extends Animal> obj9 = new ArrayList<Cat>(); //正确，编译通过 Cat 是Animal的子类
         ArrayList<? extends Animal> obj10 = new ArrayList<Dog>(); //正确，编译通过 Dog 是Animal的子类
-        ArrayList<? extends Animal> obj11 = new ArrayList<Object>(); //错误，编译失败， Object是Animal的基类
+        //ArrayList<? extends Animal> obj11 = new ArrayList<Object>(); //错误，编译失败， Object是Animal的基类
 
 
         /*
@@ -67,8 +67,8 @@ public class CollectionToFanXing {
         * */
         ArrayList<? super Animal> obj12 = new ArrayList<Object>(); //正确，编译通过， Object是Animal的基类
         ArrayList<? super Animal> obj13 = new ArrayList<Animal>(); //正确，编译通过
-        ArrayList<? super Animal> obj14 = new ArrayList<Cat>(); //错误，编译失败， Cat是Animal的子类
-        ArrayList<? super Animal> obj15 = new ArrayList<Dog>(); //错误，编译失败， Dog是Animal的子类
+        //ArrayList<? super Animal> obj14 = new ArrayList<Cat>(); //错误，编译失败， Cat是Animal的子类
+       // ArrayList<? super Animal> obj15 = new ArrayList<Dog>(); //错误，编译失败， Dog是Animal的子类
 
 
     }
